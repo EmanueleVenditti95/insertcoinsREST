c636cfb7972a18ed38d3fa5cc27ed513c78726d0
EmanueleVenditti95 <vendittiema@gmail.com>
2024-06-26 17:39:55 +0200

    add console entity with game relation

--- src/main/java/it/academylab/insertcoinsREST/entities/Gioco.java
+++ src/main/java/it/academylab/insertcoinsREST/entities/Gioco.java
@@ -1,13 +1,17 @@
 package it.academylab.insertcoinsREST.entities;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
@@ -46,5 +50,6 @@ public class Gioco {
 	@ToString.Exclude
 	private List<Commento> commenti;
 	
-	
+	@ManyToMany(fetch = FetchType.EAGER)
+    private Collection<Console> consoles = new ArrayList<>();
 }
